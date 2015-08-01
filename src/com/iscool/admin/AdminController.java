96d80d7952e936f3475d9ded68686af49db2e7e1
makhomeni <khomeni.java@gmail.com>
2015-08-01 20:24:39 +0600

    Admin Module

---
+++ src/com/iscool/admin/AdminController.java
@@ -0,0 +1,12 @@
+package com.iscool.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+    public static void main(String[] args) {
+
+    }
+}
